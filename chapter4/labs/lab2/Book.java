fcfae58b8c6247c9bb6f164b6b5734908bd80b2d
leeesoraaa <lsrlsh@naver.com>
2025-05-07 13:57:49 +0900

    chater4의 lab2 완료

--- chapter4/labs/lab2/Book.java
+++ chapter4/labs/lab2/Book.java
@@ -7,30 +7,50 @@ package chapter4.labs.lab2;
  */
 public class Book {
     // TODO: 제목, 저자, 가격, 출판연도를 저장할 수 있는 필드를 정의하세요.
-    
+    String title;
+    String author;
+    int price;
+    int year;
     
     
     // TODO: 기본 생성자를 정의하세요.
-    
+    public Book(){
+        System.out.println("Book 객체 생성(기본 생성자)");
+        this.title = "제목 없음";
+        this.author = "저자 미상";
+        this.price = 0;
+        this.year = 2025;
+    }
     
     
     // TODO: 제목만 인자로 받는 생성자를 정의하세요. (this() 사용)
-    
+    public Book(String title){this(title, "저자 미상", 0, 2025);}
     
     
     // TODO: 제목과 저자를 인자로 받는 생성자를 정의하세요. (this() 사용)
-    
+    public Book(String title, String author){
+        this(title, author, 0, 2025);
+    }
     
     
     // TODO: 제목, 저자, 가격을 인자로 받는 생성자를 정의하세요. (this() 사용)
-    
+    public Book(String title, String author, int price) {
+        this(title, author, price, 2025);
+    }
     
     
     // TODO: 모든 필드를 인자로 받는 생성자를 정의하세요.
-    
+    public Book(String title, String author, int price, int year){
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.year = year;
+    }
     
     
     // TODO: 책의 정보를 출력하는 메소드를 정의하세요.
-    
+    public void getInfo(){
+        System.out.println("제목: "+title+"  작가: "+author+"  가격: "+price+"  출판연도: "+year);
+    }
     
 } 
--- chapter4/labs/lab2/BookTest.java
+++ chapter4/labs/lab2/BookTest.java
@@ -10,27 +10,35 @@ public class BookTest {
         System.out.println("Book 클래스 테스트를 시작합니다.");
         
         // TODO: 기본 생성자를 사용하여 Book 객체를 생성하고 정보를 출력하세요.
-        
-        
+        Book book1 = new Book();
+        book1.getInfo();
+        System.out.println();
         
         // TODO: 제목만 인자로 받는 생성자를 사용하여 Book 객체를 생성하고 정보를 출력하세요.
-        
-        
+        Book book2 = new Book("AWS 클라우드 활용");
+        book2.getInfo();
+        System.out.println();
         
         // TODO: 제목과 저자를 인자로 받는 생성자를 사용하여 Book 객체를 생성하고 정보를 출력하세요.
-        
+        Book book3 = new Book("MSA 기반", "이소라");
+        book3.getInfo();
+        System.out.println();
         
         
         // TODO: 제목, 저자, 가격을 인자로 받는 생성자를 사용하여 Book 객체를 생성하고 정보를 출력하세요.
-        
-        
+        Book book4 = new Book("Java 개발자", "Java master", 10000);
+        book4.getInfo();
+        System.out.println();
         
         // TODO: 모든 필드를 인자로 받는 생성자를 사용하여 Book 객체를 생성하고 정보를 출력하세요.
-        
-        
+        Book book5 = new Book("새싹 영등포캠 7기","국제인재능력개발원", 200000, 2025);
+        book5.getInfo();
+        System.out.println();
         
         // TODO: 객체를 생성한 후 필드 값을 변경하고 정보를 출력하세요.
-        
+        Book book6 = new Book("청년취업사관학교", "이소라", 50000, 2024);
+        book6.author = "저자 미상";
+        book6.getInfo();
         
     }
 } 
