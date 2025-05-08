ded3a50fc0fd60ff30afa025c7d4b9fd858a7ad5
leeesoraaa <lsrlsh@naver.com>
2025-05-08 19:31:27 +0900

    chapter6의 lab2 완료

--- chapter6/labs/lab2/FileIOExceptionLab.java
+++ chapter6/labs/lab2/FileIOExceptionLab.java
@@ -1,6 +1,7 @@
 package chapter6.labs.lab2;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
@@ -18,7 +19,8 @@ public class FileIOExceptionLab {
         Scanner scanner = new Scanner(System.in);
         
         // TODO: 사용자로부터 읽을 파일 이름을 입력받으세요.
-        
+        System.out.println("파일명: ");
+        String fileName = scanner.nextLine();
         
         // TODO: try-with-resources 구문을 사용하여 파일을 읽고 내용을 분석하세요.
         // 1. BufferedReader를 사용하여 파일을 한 줄씩 읽습니다.
@@ -31,8 +33,34 @@ public class FileIOExceptionLab {
         
         
         // 참고: 단어 수 계산을 위해 String.split("\\s+") 메소드를 활용할 수 있습니다.
-        
-        
+
+        int totalLine = 0;
+        int totalWord = 0;
+        int totalChar = 0;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))){
+            String line = null;
+            while((line = br.readLine()) !=null){
+                totalLine++;
+                totalChar+=line.length();
+                totalWord += line.trim().isEmpty()?0:line.trim().length();
+                System.out.println(line);
+                System.out.println("글자 수: "+line.length()+"  단어 수: "+line.split("\\s+").length);
+            }
+            System.out.println("전체 줄 수: "+totalLine);
+            System.out.println("전체 단어 수: "+totalWord);
+            System.out.println("전체 글자 수: "+totalChar);
+        } catch(FileNotFoundException e) {
+            System.out.println("존재하지 않는 파일입니다.");
+            System.out.println(e.getMessage());
+        } catch (IOException e){
+            System.out.println("파일을 읽는 중 오류가 발생했습니다.");
+            System.out.println(e.getMessage());
+        } catch (Exception e) {
+            System.out.println("기타 예외 발생");
+            System.out.println(e.getMessage());
+        }
+        System.out.println();
         scanner.close();
         System.out.println("프로그램을 종료합니다.");
     }
